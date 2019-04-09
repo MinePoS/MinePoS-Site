a8f30dad82fd5956f732b66df2d7655e593eb022
Keystirras <keystirras@yahoo.com>
2019-04-09 00:22:33 -0600

    Fixed main page

---
+++ includes/header.php
@@ -0,0 +1,53 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | MinePoS</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/material-icons.min.css">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Aladin">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Amaranth">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+    <link rel="icon" href="favicon.ico">
+    <meta property="og:title" content="MinePoS" />
+    <meta property="og:url" content="https://MinePoS.net/" />
+    <meta property="og:image" content="https://minepos.net/assets/img/logo.png" />
+    <meta property="og:description" content="MinePoS is a free self-hosted game donation system" />
+    <meta name="theme-color" content="#A32A2A">
+
+</head>
+
+<body style="background-color:rgb(98,25,25);">
+
+<nav class="navbar navbar-expand-md navbar-dark sticky-top navigation-clean-button">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php"> <img src="assets/img/MinePosLogo.svg" class="minepos-nav"> </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link navsize" href="index.php"><i class="fa fa-home"></i> Home</a>
+                </li>
+                <li class="nav-item navsize">
+                    <a class="nav-link" href="construction.html"><i class="fa fa-star-o"></i> Features</a>
+                </li>
+                <li class="nav-item navsize">
+                    <a class="nav-link" href="construction.html"><i class="fas fa-hand-holding-heart"></i> Our Mission</a>
+                </li>
+                <li class="nav-item navsize">
+                    <a class="nav-link" href="https://github.com/MinePoS/Documentation/wiki"><i class="fas fa-book-open"></i> Docs</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,93 @@
+<?php include 'includes/header.php' ?>
+    <div class="jumbotron bg-shake" style="background-image:url(&quot;assets/img/cube-bg.png&quot;);background-color:rgb(98,25,25);height:500px;">
+        <div class="col">
+            <h1 style="color:rgb(255,255,255);padding:20px;font-family:Aladin, cursive;font-size:70px;">Make money using MinePos!</h1>
+        </div>
+        <div class="col">
+            <h1 style="color:rgb(255,255,255);padding:20px;font-family:Aladin, cursive;">Best of all? It's <span style="text-decoration: underline;">FREE!</span></h1>
+        </div>
+        <a class="btn btn-danger add" style="width: 11%" href="https://github.com/MinePoS/Documentation/wiki" role="button">Start Your Journey!</a>
+    </div>
+    <div class="testimonials-clean" style="background-color:#252830;">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center" style="font-family:Amaranth, sans-serif;color:rgb(255,255,255);">Testimonials </h2>
+                <p class="text-center" style="color:rgb(255,199,0);padding:10px;">To our beloved MinePos users,&nbsp;<br>We'd like to thank you for using this free software.&nbsp;<br>Here are what some of you have to say&nbsp;</p>
+            </div>
+            <div class="row people">
+                <div class="col-md-6 col-lg-4 item">
+                    <div class="box">
+                        <p class="description">MinePos was the superior option for me as finance isn't always easy with a startup. Overall a great help and worry free.</p>
+                    </div>
+                    <div class="author"><img class="rounded-circle" src="assets/img/unknown.png">
+                        <h5 class="name" style="color:rgb(255,255,255);">DiscoverSquishy</h5>
+                        <p class="title">CEO of CraftiteMania</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 item">
+                    <div class="box">
+                        <p class="description">As the main creator and developer of MinePos I enjoy using my own software for personal purposes like test servers etc.</p>
+                    </div>
+                    <div class="author"><img class="rounded-circle" src="assets/img/Andrew.png">
+                        <h5 class="name" style="color:rgb(255,255,255);">Andrew Aubury</h5>
+                        <p class="title">CEO of MinePos&nbsp;</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 item">
+                    <div class="box">
+                        <p class="description">Creating a server is hard enough and with the extra charges of those minecraft stores it's a big struggle MinePos just helps me calm my head.</p>
+                    </div>
+                    <div class="author"><img class="rounded-circle" src="assets/img/Craig.png">
+                        <h5 class="name" style="color:rgb(255,255,255);">Craig&nbsp;</h5>
+                        <p class="title">Owner of Dubcraft</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="features-boxed" style="background-color:rgb(241,250,255);">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center" style="color:rgb(0,0,0);">Features </h2>
+                <p class="text-center" style="color:rgb(0,0,0);height:200px;"><em>"But there are hundreds of free store panels out there what makes this one better than the others?"&nbsp;</em><br>-<br><strong>Well, unlike other panels we're all about security and&nbsp;customizability we update our panel regularly with new features security improvments or implement your amazing suggustions. </strong><br><br><strong>Still not convinced? Well here's a little preview of what we offer.</strong></p>
+            </div>
+            <div class="row justify-content-center features">
+                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                    <div class="box"><i class="fa fa-cloud icon"></i>
+                        <h3 class="name">Self Hosted</h3>
+                        <a href="construction.html" class="learn-more">Learn more »</a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                    <div class="box"><i class="fa fa-support icon"></i>
+                        <h3 class="name">24/7 Support</h3>
+                        <a href="construction.html" class="learn-more">Learn more »</a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                    <div class="box"><i class="fa fa-code-fork icon"></i>
+                        <h3 class="name">Customizable </h3>
+                        <a href="construction.html" class="learn-more">Learn more »</a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                    <div class="box"><i class="material-icons icon">high_quality</i>
+                        <h3 class="name">Guaranteed Quality</h3>
+                        <a href="construction.html" class="learn-more">Learn more »</a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                    <div class="box"><i class="fa fa-tachometer icon"></i>
+                        <h3 class="name">Blazing Fast</h3>
+                        <a href="construction.html" class="learn-more">Learn more »</a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                    <div class="box"><i class="fa fa-github-square icon"></i>
+                        <h3 class="name">Open Source</h3><a href="construction.html" class="learn-more">Learn more »</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php include 'includes/footer.php' ?>
