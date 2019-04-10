93c838db9f6ac2c2f4216814e983c805ad1c5d0a
Keystirras <keystirras@yahoo.com>
2019-04-10 01:08:01 -0600

    Oof

--- index.php
+++ index.php
@@ -112,7 +112,7 @@ include 'includes/header.php';
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="customLabel">Modal title</h5>
+        <h5 class="modal-title" id="customLabel">Customization</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
