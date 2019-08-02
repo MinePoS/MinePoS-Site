94665184e1c3e5071bfb95fa4d3db252fafb87a4
Keystirras <keystirras@yahoo.com>
2019-08-01 19:31:52 -0600

    Updated CDN

--- features.php
+++ features.php
@@ -20,7 +20,7 @@ include 'php/header.php'; ?>
 
                     <div class="row align-items-center pt-4 appear-animation animated fadeInLeftShorter appear-animation-visible" data-appear-animation="fadeInLeftShorter" style="animation-delay: 100ms;">
                         <div class="col-md-4 mb-4 mb-md-0">
-                            <img class="img-fluid scale-6 pr-5 pr-md-0 my-4" src="http://minepos.b-cdn.net/img/Browser%20Mockup.png" alt="Sleek Layout">
+                            <img class="img-fluid scale-6 pr-5 pr-md-0 my-4" src="https://minepos.b-cdn.net/img/Browser%20Mockup.png" alt="Sleek Layout">
                         </div>
                         <div class="col-md-8 pl-md-5">
                             <h2 class="font-weight-normal text-6 mb-3"><strong class="font-weight-extra-bold">Sleek</strong> Layout</h2>
@@ -36,13 +36,13 @@ include 'php/header.php'; ?>
                             <p class="text-4">We make it easy to view and manage all the orders your players make. With our Order section of our panel, you can add, remove, and edit orders by your players.</p>
                         </div>
                         <div class="col-md-4 px-5 px-md-3">
-                            <img class="img-fluid scale-6 my-4" src="http://minepos.b-cdn.net/img/Browser%20Mockup(1).png" alt="Order Management">
+                            <img class="img-fluid scale-6 my-4" src="https://minepos.b-cdn.net/img/Browser%20Mockup(1).png" alt="Order Management">
                         </div>
                     </div>
                     <hr class="solid my-5">
                     <div class="row align-items-center pt-4 appear-animation animated fadeInLeftShorter appear-animation-visible" data-appear-animation="fadeInLeftShorter" style="animation-delay: 100ms;">
                         <div class="col-md-4 mb-4 mb-md-0">
-                            <img class="img-fluid scale-6 pr-5 pr-md-0 my-4" src="http://minepos.b-cdn.net/img/Browser%20Mockup(2).png" alt="User Management">
+                            <img class="img-fluid scale-6 pr-5 pr-md-0 my-4" src="https://minepos.b-cdn.net/img/Browser%20Mockup(2).png" alt="User Management">
                         </div>
                         <div class="col-md-8 pl-md-5">
                             <h2 class="font-weight-normal text-6 mb-3"><strong class="font-weight-extra-bold">User</strong> Management</h2>
