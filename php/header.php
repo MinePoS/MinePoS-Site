802bc73df48947e711af15882866ad0f0f70637e
Keystirras <keystirras@yahoo.com>
2019-08-01 19:03:39 -0600

    Update for ssl cert

--- php/header.php
+++ php/header.php
@@ -13,16 +13,16 @@
     <meta name="author" content="Keystirras.ml">
 
     <!-- Favicon -->
-    <link rel="shortcut icon" href="http://minepos.b-cdn.net/favicon.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="http://minepos.b-cdn.net/img/icons/apple-touch-icon.png" />
-    <link rel="apple-touch-icon" sizes="57x57" href="http://minepos.b-cdn.net/img/icons/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="http://minepos.b-cdn.net/img/icons/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="76x76" href="http://minepos.b-cdn.net/img/icons/apple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="http://minepos.b-cdn.net/img/icons/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon" sizes="120x120" href="http://minepos.b-cdn.net/img/icons/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="http://minepos.b-cdn.net/img/icons/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon" sizes="152x152" href="http://minepos.b-cdn.net/img/icons/apple-touch-icon-152x152.png" />
-    <link rel="apple-touch-icon" sizes="180x180" href="http://minepos.b-cdn.net/img/icons/apple-touch-icon-180x180.png" />
+    <link rel="shortcut icon" href="https://minepos.b-cdn.net/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="https://minepos.b-cdn.net/img/icons/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="https://minepos.b-cdn.net/img/icons/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="https://minepos.b-cdn.net/img/icons/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="https://minepos.b-cdn.net/img/icons/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="https://minepos.b-cdn.net/img/icons/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="https://minepos.b-cdn.net/img/icons/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="https://minepos.b-cdn.net/img/icons/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="https://minepos.b-cdn.net/img/icons/apple-touch-icon-152x152.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="https://minepos.b-cdn.net/img/icons/apple-touch-icon-180x180.png" />
 
     <!-- Mobile Metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
@@ -35,35 +35,35 @@
 <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Lemon" rel="stylesheet"> 
 
     <!-- Vendor CSS -->
-    <link rel="stylesheet" href="http://minepos.b-cdn.net/vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="http://minepos.b-cdn.net/vendor/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="http://minepos.b-cdn.net/vendor/animate/animate.min.css">
-    <link rel="stylesheet" href="http://minepos.b-cdn.net/vendor/simple-line-icons/css/simple-line-icons.min.css">
-    <link rel="stylesheet" href="http://minepos.b-cdn.net/vendor/owl.carousel/assets/owl.carousel.min.css">
-    <link rel="stylesheet" href="http://minepos.b-cdn.net/vendor/owl.carousel/assets/owl.theme.default.min.css">
-    <link rel="stylesheet" href="http://minepos.b-cdn.net/vendor/magnific-popup/magnific-popup.min.css">
+    <link rel="stylesheet" href="https://minepos.b-cdn.net/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://minepos.b-cdn.net/vendor/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="https://minepos.b-cdn.net/vendor/animate/animate.min.css">
+    <link rel="stylesheet" href="https://minepos.b-cdn.net/vendor/simple-line-icons/css/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://minepos.b-cdn.net/vendor/owl.carousel/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://minepos.b-cdn.net/vendor/owl.carousel/assets/owl.theme.default.min.css">
+    <link rel="stylesheet" href="https://minepos.b-cdn.net/vendor/magnific-popup/magnific-popup.min.css">
 
     <!-- Theme CSS -->
-    <link rel="stylesheet" href="http://minepos.b-cdn.net/css/theme.css">
-    <link rel="stylesheet" href="http://minepos.b-cdn.net/css/theme-elements.css">
+    <link rel="stylesheet" href="https://minepos.b-cdn.net/css/theme.css">
+    <link rel="stylesheet" href="https://minepos.b-cdn.net/css/theme-elements.css">
 
     <!-- Current Page CSS -->
-    <link rel="stylesheet" href="http://minepos.b-cdn.net/vendor/rs-plugin/css/settings.css">
-    <link rel="stylesheet" href="http://minepos.b-cdn.net/vendor/rs-plugin/css/layers.css">
-    <link rel="stylesheet" href="http://minepos.b-cdn.net/vendor/rs-plugin/css/navigation.css">
-    <link rel="stylesheet" href="http://minepos.b-cdn.net/vendor/circle-flip-slideshow/css/component.css">
+    <link rel="stylesheet" href="https://minepos.b-cdn.net/vendor/rs-plugin/css/settings.css">
+    <link rel="stylesheet" href="https://minepos.b-cdn.net/vendor/rs-plugin/css/layers.css">
+    <link rel="stylesheet" href="https://minepos.b-cdn.net/vendor/rs-plugin/css/navigation.css">
+    <link rel="stylesheet" href="https://minepos.b-cdn.net/vendor/circle-flip-slideshow/css/component.css">
 
     <!-- Demo CSS -->
 
 
     <!-- Skin CSS -->
-    <link rel="stylesheet" href="http://minepos.b-cdn.net/css/skins/default.css">
+    <link rel="stylesheet" href="https://minepos.b-cdn.net/css/skins/default.css">
 
     <!-- Theme Custom CSS -->
-    <link rel="stylesheet" href="http://minepos.b-cdn.net/css/custom.css">
+    <link rel="stylesheet" href="https://minepos.b-cdn.net/css/custom.css">
 
     <!-- Head Libs -->
-    <script src="http://minepos.b-cdn.net/vendor/modernizr/modernizr.min.js"></script>
+    <script src="https://minepos.b-cdn.net/vendor/modernizr/modernizr.min.js"></script>
 
 </head>
 <body>
