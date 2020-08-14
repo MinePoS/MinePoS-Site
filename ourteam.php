09bdc00ed7b492a37ba0606d0d527fc561c394b3
Ian H <hessi@hessapps.com>
2020-08-14 16:15:31 -0400

    Updates :/

--- ourteam.php
+++ ourteam.php
@@ -74,6 +74,7 @@ include 'php/header.php'; ?>
             </div>
         </div>
     </div>
+	
     <div class="row">
         <div class="col">
             <hr class="solid my-5">
@@ -112,14 +113,16 @@ include 'php/header.php'; ?>
             <hr class="solid my-5">
         </div>
     </div>
-
     <div class="row">
+        <div class="col-md-5 order-md-2 mb-4 mb-lg-0 appear-animation animated fadeInLeftShorter appear-animation-visible" data-appear-animation="fadeInLeftShorter" style="animation-delay: 100ms;">
+            <img src="https://minepos.b-cdn.net/img/Kyle.png" style="max-height: 300px" class="img-fluid" alt="">
+        </div>
         <div class="col-md-7 order-2">
             <div class="overflow-hidden">
                 <h2 class="text-color-dark font-weight-bold text-8 mb-0 pt-0 mt-0 appear-animation animated maskUp appear-animation-visible" data-appear-animation="maskUp" data-appear-animation-delay="300" style="animation-delay: 300ms;">Kyle Talamantez</h2>
             </div>
             <div class="overflow-hidden mb-3">
-                <p class="font-weight-bold text-primary text-uppercase mb-0 appear-animation animated maskUp appear-animation-visible" data-appear-animation="maskUp" data-appear-animation-delay="300" style="animation-delay: 300ms;">Web Developer / Support</p>
+                <p class="font-weight-bold text-primary text-uppercase mb-0 appear-animation animated maskUp appear-animation-visible" data-appear-animation="maskUp" data-appear-animation-delay="500" style="animation-delay: 500ms;">Web Developer / Support</p>
             </div>
             <p class="lead appear-animation animated fadeInUpShorter appear-animation-visible" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="700" style="animation-delay: 700ms;">Kyle works as a cook in a rehabilitation center in the United States, with experience in management. For a pass time, he likes to develop and design websites, listen to music, and play World of Warcraft. Kyle is always trying to learn more about website development through online classes when he has a little free time. </p>
             <hr class="solid my-4 appear-animation animated fadeInUpShorter appear-animation-visible" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="900" style="animation-delay: 900ms;">
@@ -136,10 +139,8 @@ include 'php/header.php'; ?>
                 </div>
             </div>
         </div>
-        <div class="col-md-5 order-md-2 mb-4 mb-lg-0 appear-animation animated fadeInRightShorter appear-animation-visible" data-appear-animation="fadeInRightShorter" style="animation-delay: 100ms;">
-            <img src="https://minepos.b-cdn.net/img/Kyle.png" class="img-fluid" alt="">
-        </div>
     </div>
+
 
 </div>
 <?php include 'php/footer.php' ?>
